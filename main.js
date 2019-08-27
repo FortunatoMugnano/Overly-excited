b445615cade7aa4da92ae6e792346fb147de8eb5
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-08-27 10:33:53 -0500

    My first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const cakes = [
+    zebraCake = {
+    colors: ["white", "brown"],
+    numberOfRolls: 6,
+    price: 1.99,
+    ingredients: ["Chocolate", "Cream", "Milk"],
+    motto: "Get some Stripes!",
+    seasonal: true
+},
+americanCake = {
+    colors: ["white", "red"],
+    numberOfRolls: 6,
+    price: 2.19,
+    ingredients: ["Strowberry", "Cream", "Milk", "Blueberry"],
+    motto: "Red white and Blueberry!",
+    seasonal: true
+},
+christmasCake = {
+    colors: ["white", "brown"],
+    numberOfRolls: 6,
+    price: 2.19,
+    ingredients: ["Eggnog", "Cream", "Milk"],
+    motto: "Eggnog cake Rolls!",
+    seasonal: true
+
+}];
+
+console.log("Tell me about the cake", cakes);
